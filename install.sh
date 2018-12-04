31bccdf106da5bc8e968de5e18b05facc390debb
Simon Massey <simon@simonmassey.org>
2018-12-04 20:48:18 +0000

    upgraded to helmfile

--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 set -e
+set -x
 
-helm upgrade --install \
-    --set "helmVersion=2.8.1,project=realworld,insecureSkipTlsVerify=true,webhookRepFullname=simbo1905/demo1,envGitUrl=https://github.com/ocd-scm/ocd-demo-env-build.git" \
-    ocd-environment-webhook \
-    ocd-environment-webhook-1.0.0.tgz
-
-# oc expose service/ocd-builder-webhook
-
-# PROJECT=$(oc project --short)
-# echo $PROJECT
-
-# HOSTNAME=$(oc get routes | grep ocd-builder-webhook | awk '{print $2}' | sed "s/^[^\.]*/gitea-$PROJECT/1")
-# echo $HOSTNAME
-
-# # install gitea https://github.com/wkulhanek/docker-openshift-gitea
-# oc new-app -f https://raw.githubusercontent.com/wkulhanek/docker-openshift-gitea/master/openshift/gitea-persistent-template.yaml --param=HOSTNAME=$HOSTNAME
+(
+set -a
+OPENSHIFT_SERVER=192.168.99.100
+OPENSHIFT_USER=admin
+OPENSHIFT_PASSWORD=admin
+helmfile apply
+set +a
+oc expose service/ocd-environment-webhook
+)
