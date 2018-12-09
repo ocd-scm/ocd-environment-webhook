60dbbf5c8e6c630bcd62a832596c1a0f7a3618ab
Simon Massey <simon@simonmassey.org>
2018-12-09 07:42:22 +0000

    delete secrets

---
+++ bin/ocd-delete-secrets.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# find all the the *secret files and delete the unecrypted version
+find ${OCD_CHECKOUT_PATH} -type f -name '*secret' | while read -r SECRET ; do 
+    if [ -f "${SECRET%.*}" ]; then
+        rm "${SECRET%.*}"
+    fi
+done
