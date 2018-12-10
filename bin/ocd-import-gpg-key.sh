fb64f44363d6dac72b0c095530c8acbd1e2cbb7f
Simon Massey <simon@simonmassey.org>
2018-12-10 21:57:59 +0000

    error handling

--- bin/ocd-import-gpg-key.sh
+++ bin/ocd-import-gpg-key.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
 # this wrapper will detected session timeout and login
-
 oc() { 
     ${OCD_SCRIPTS_PATH}/oc_wrapper.sh "$@" 
 }
 
+# logic below assumes one private key and many sounds wrong
 if [ "$(ls gpg/*.prv.key | wc -l)" -ne "1" ]; then
     >2& echo "more than one prv.key found at gpg/*.prv.key"
     exit 1
 fi
 
+# see https://github.com/ocd-scm/ocd-meta/wiki/Encrypting-Secrets#using-a-gnupg-private-key
 # parse the fingerprint out of the private key name
 FINGERPRINT=$(ls gpg/*.prv.key | sed 's/gpg\/\(.*\)\.prv\.key/\1/g')
 
-# check the key is there
+# check the key has alredy been imported
 gpg --list-secret-keys | grep $FINGERPRINT
 
+# if the key hasn't been imported load the key passphrase from the environment
 if [ "$?" -ne "0" ]; then
     oc project $PROJECT 
+    # see https://github.com/ocd-scm/ocd-meta/wiki/Encrypting-Secrets#using-a-gnupg-private-key
     PASSPHRASE=$(oc get secrets openshift-passphrase -o yaml | grep passphrase: | awk '{print $2}' | base64 --decode)
-    KEY=$(find ${OCD_CHECKOUT_PATH} -name ocd-private.key)
     echo $PASSPHRASE | gpg --pinentry loopback --import --passphrase-fd 0 $(ls gpg/*.prv.key)
     # check the key is there
     gpg --list-secret-keys | grep $FINGERPRINT
     if [ "$?" -ne "0" ]; then
-        >&2 echo "Could not import key from $(ls gpg/*.prv.key)"
+        >&2 echo "ERROR: Could not import key from $(ls gpg/*.prv.key)"
+        exit 2
     fi
 fi
