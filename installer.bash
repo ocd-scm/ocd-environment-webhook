58fb93828d30121eae8615cfb150e3b7f5cbb088
Simon Massey <simon@simonmassey.org>
2018-12-11 19:58:12 +0000

    mandatory args are now positional

--- installer.bash
+++ installer.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Created by argbash-init v2.7.1
-# ARG_OPTIONAL_SINGLE([oc-server],[s],[mandatory e.g. 192.168.99.100:8443])
-# ARG_OPTIONAL_SINGLE([oc-user],[u],[mandatory username e.g. admin])
-# ARG_OPTIONAL_SINGLE([oc-passwd],[p],[mandatory password e.g. admin])
-# ARG_OPTIONAL_SINGLE([tiller-namespace],[t],[mandatory namespace where tiller is running])
-# ARG_OPTIONAL_SINGLE([namespace],[n],[mandatory namespace to instal into])
-# ARG_OPTIONAL_SINGLE([insecure-no-tls-verify],[],[optional skip TLS verify needed for minishift])
+# ARG_OPTIONAL_SINGLE([insecure-no-tls-verify],[],[skip TLS verify needed for minishift],[false])
+# ARG_POSITIONAL_SINGLE([oc-server],[mandatory server e.g. 192.168.99.100:8443])
+# ARG_POSITIONAL_SINGLE([oc-user],[mandatory username e.g. admin])
+# ARG_POSITIONAL_SINGLE([oc-passwd],[mandatory password e.g. admin])
+# ARG_POSITIONAL_SINGLE([tiller-namespace],[mandatory namespace where tiller is running])
+# ARG_POSITIONAL_SINGLE([namespace],[mandatory namespace to install into])
 # ARG_POSITIONAL_SINGLE([git-url],[mandatory url of the enviroment code to checkout e.g. https://github.com/ocd-scm/ocd-demo-env-build.git ])
 # ARG_POSITIONAL_SINGLE([git-name],[mandatory name of the repo that fires the webhook used to sanity check the webhook payload is from the correct repo e.g. ocd-scm/ocd-demo-env-build])
 # ARG_POSITIONAL_SINGLE([webhook-ref],[mandatory the git ref that fires the webhook e.g. refs/heads/master])
+# ARG_DEFAULTS_POS()
 # ARG_HELP([Welcome to the ocd-envrionment-webhook installer. It runs heml to install into the current project. It needs some OC login details as tokens will expire so it will have to periodically login to refresh it's authentication token. It needs to know the namespace where tiller is running which might not be the current project. The login you give it will need permissions to list the pods where tiller is running and to port forward to it. On minishift you can use the admin plugin and just have it use admin/admin. In a secure setup you should run this in a project seperate from both tiller and your main app with a login that can only talk to tiller and nothing else.])
 # ARGBASH_GO()
 # needed because of Argbash --> m4_ignore([
@@ -30,35 +31,38 @@ die()
 
 begins_with_short_option()
 {
-	local first_option all_short_options='suptnh'
+	local first_option all_short_options='h'
 	first_option="${1:0:1}"
 	test "$all_short_options" = "${all_short_options/$first_option/}" && return 1 || return 0
 }
 
 # THE DEFAULTS INITIALIZATION - POSITIONALS
 _positionals=()
-# THE DEFAULTS INITIALIZATION - OPTIONALS
 _arg_oc_server=
 _arg_oc_user=
 _arg_oc_passwd=
 _arg_tiller_namespace=
 _arg_namespace=
-_arg_insecure_no_tls_verify=
+_arg_git_url=
+_arg_git_name=
+_arg_webhook_ref=
+# THE DEFAULTS INITIALIZATION - OPTIONALS
+_arg_insecure_no_tls_verify="false"
 
 
 print_help()
 {
 	printf '%s\n' "Welcome to the ocd-envrionment-webhook installer. It runs heml to install into the current project. It needs some OC login details as tokens will expire so it will have to periodically login to refresh it's authentication token. It needs to know the namespace where tiller is running which might not be the current project. The login you give it will need permissions to list the pods where tiller is running and to port forward to it. On minishift you can use the admin plugin and just have it use admin/admin. In a secure setup you should run this in a project seperate from both tiller and your main app with a login that can only talk to tiller and nothing else."
-	printf 'Usage: %s [-s|--oc-server <arg>] [-u|--oc-user <arg>] [-p|--oc-passwd <arg>] [-t|--tiller-namespace <arg>] [-n|--namespace <arg>] [--insecure-no-tls-verify <arg>] [-h|--help] <git-url> <git-name> <webhook-ref>\n' "$0"
+	printf 'Usage: %s [--insecure-no-tls-verify <arg>] [-h|--help] <oc-server> <oc-user> <oc-passwd> <tiller-namespace> <namespace> <git-url> <git-name> <webhook-ref>\n' "$0"
+	printf '\t%s\n' "<oc-server>: mandatory server e.g. 192.168.99.100:8443"
+	printf '\t%s\n' "<oc-user>: mandatory username e.g. admin"
+	printf '\t%s\n' "<oc-passwd>: mandatory password e.g. admin"
+	printf '\t%s\n' "<tiller-namespace>: mandatory namespace where tiller is running"
+	printf '\t%s\n' "<namespace>: mandatory namespace to install into"
 	printf '\t%s\n' "<git-url>: mandatory url of the enviroment code to checkout e.g. https://github.com/ocd-scm/ocd-demo-env-build.git "
 	printf '\t%s\n' "<git-name>: mandatory name of the repo that fires the webhook used to sanity check the webhook payload is from the correct repo e.g. ocd-scm/ocd-demo-env-build"
 	printf '\t%s\n' "<webhook-ref>: mandatory the git ref that fires the webhook e.g. refs/heads/master"
-	printf '\t%s\n' "-s, --oc-server: mandatory e.g. 192.168.99.100:8443 (no default)"
-	printf '\t%s\n' "-u, --oc-user: mandatory username e.g. admin (no default)"
-	printf '\t%s\n' "-p, --oc-passwd: mandatory password e.g. admin (no default)"
-	printf '\t%s\n' "-t, --tiller-namespace: mandatory namespace where tiller is running (no default)"
-	printf '\t%s\n' "-n, --namespace: mandatory namespace to instal into (no default)"
-	printf '\t%s\n' "--insecure-no-tls-verify: optional skip TLS verify needed for minishift (no default)"
+	printf '\t%s\n' "--insecure-no-tls-verify: skip TLS verify needed for minishift (default: 'false')"
 	printf '\t%s\n' "-h, --help: Prints help"
 }
 
@@ -70,61 +74,6 @@ parse_commandline()
 	do
 		_key="$1"
 		case "$_key" in
-			-s|--oc-server)
-				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
-				_arg_oc_server="$2"
-				shift
-				;;
-			--oc-server=*)
-				_arg_oc_server="${_key##--oc-server=}"
-				;;
-			-s*)
-				_arg_oc_server="${_key##-s}"
-				;;
-			-u|--oc-user)
-				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
-				_arg_oc_user="$2"
-				shift
-				;;
-			--oc-user=*)
-				_arg_oc_user="${_key##--oc-user=}"
-				;;
-			-u*)
-				_arg_oc_user="${_key##-u}"
-				;;
-			-p|--oc-passwd)
-				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
-				_arg_oc_passwd="$2"
-				shift
-				;;
-			--oc-passwd=*)
-				_arg_oc_passwd="${_key##--oc-passwd=}"
-				;;
-			-p*)
-				_arg_oc_passwd="${_key##-p}"
-				;;
-			-t|--tiller-namespace)
-				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
-				_arg_tiller_namespace="$2"
-				shift
-				;;
-			--tiller-namespace=*)
-				_arg_tiller_namespace="${_key##--tiller-namespace=}"
-				;;
-			-t*)
-				_arg_tiller_namespace="${_key##-t}"
-				;;
-			-n|--namespace)
-				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
-				_arg_namespace="$2"
-				shift
-				;;
-			--namespace=*)
-				_arg_namespace="${_key##--namespace=}"
-				;;
-			-n*)
-				_arg_namespace="${_key##-n}"
-				;;
 			--insecure-no-tls-verify)
 				test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
 				_arg_insecure_no_tls_verify="$2"
@@ -154,16 +103,16 @@ parse_commandline()
 
 handle_passed_args_count()
 {
-	local _required_args_string="'git-url', 'git-name' and 'webhook-ref'"
-	test "${_positionals_count}" -ge 3 || _PRINT_HELP=yes die "FATAL ERROR: Not enough positional arguments - we require exactly 3 (namely: $_required_args_string), but got only ${_positionals_count}." 1
-	test "${_positionals_count}" -le 3 || _PRINT_HELP=yes die "FATAL ERROR: There were spurious positional arguments --- we expect exactly 3 (namely: $_required_args_string), but got ${_positionals_count} (the last one was: '${_last_positional}')." 1
+	local _required_args_string="'oc-server', 'oc-user', 'oc-passwd', 'tiller-namespace', 'namespace', 'git-url', 'git-name' and 'webhook-ref'"
+	test "${_positionals_count}" -ge 8 || _PRINT_HELP=yes die "FATAL ERROR: Not enough positional arguments - we require exactly 8 (namely: $_required_args_string), but got only ${_positionals_count}." 1
+	test "${_positionals_count}" -le 8 || _PRINT_HELP=yes die "FATAL ERROR: There were spurious positional arguments --- we expect exactly 8 (namely: $_required_args_string), but got ${_positionals_count} (the last one was: '${_last_positional}')." 1
 }
 
 
 assign_positional_args()
 {
 	local _positional_name _shift_for=$1
-	_positional_names="_arg_git_url _arg_git_name _arg_webhook_ref "
+	_positional_names="_arg_oc_server _arg_oc_user _arg_oc_passwd _arg_tiller_namespace _arg_namespace _arg_git_url _arg_git_name _arg_webhook_ref "
 
 	shift "$_shift_for"
 	for _positional_name in ${_positional_names}
@@ -184,6 +133,16 @@ assign_positional_args 1 "${_positionals[@]}"
 # [ <-- needed because of Argbash
 
 
-source ./install.sh
+printf 'Value of --%s: %s\n' 'insecure-no-tls-verify' "$_arg_insecure_no_tls_verify"
+printf "Value of '%s': %s\\n" 'oc-server' "$_arg_oc_server"
+printf "Value of '%s': %s\\n" 'oc-user' "$_arg_oc_user"
+printf "Value of '%s': %s\\n" 'oc-passwd' "$_arg_oc_passwd"
+printf "Value of '%s': %s\\n" 'tiller-namespace' "$_arg_tiller_namespace"
+printf "Value of '%s': %s\\n" 'namespace' "$_arg_namespace"
+printf "Value of '%s': %s\\n" 'git-url' "$_arg_git_url"
+printf "Value of '%s': %s\\n" 'git-name' "$_arg_git_name"
+printf "Value of '%s': %s\\n" 'tag-regex' "$_arg_tag_regex"
+
+. ./install.sh
 
 # ] <-- needed because of Argbash
