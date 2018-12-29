06151fe5608e612d7d29064ceb7aec6a8290871f
Simon Massey <simon@simonmassey.org>
2018-12-29 19:41:52 +0000

    use relative path

--- bin/ocd-import-gpg-key.sh
+++ bin/ocd-import-gpg-key.sh
@@ -6,14 +6,14 @@ oc() {
 }
 
 # later logic assumes one private key and many sounds wrong
-if [ "$(ls gpg/*.prv.key | wc -l)" -ne "1" ]; then
+if [ "$(find ${OCD_CHECKOUT_PATH} -type f -name \*.prv.key | wc -l)" -ne "1" ]; then
     >&2 echo "ERROR Did not find exactly one file at gpg/*.prv.key"
     exit 1
 fi
 
 # see https://github.com/ocd-scm/ocd-meta/wiki/Encrypting-Secrets#using-a-gnupg-private-key
 # parse the fingerprint out of the private key name
-FINGERPRINT=$(ls gpg/*.prv.key | sed 's/gpg\/\(.*\)\.prv\.key/\1/g')
+FINGERPRINT=$(find ${OCD_CHECKOUT_PATH} -type f -name \*.prv.key | sed 's/gpg\/\(.*\)\.prv\.key/\1/g')
 
 # check the key has alredy been imported
 gpg --list-secret-keys | grep $FINGERPRINT
@@ -23,11 +23,11 @@ if [ "$?" -ne "0" ]; then
     oc project $PROJECT 
     # see https://github.com/ocd-scm/ocd-meta/wiki/Encrypting-Secrets#using-a-gnupg-private-key
     PASSPHRASE=$(oc get secrets openshift-passphrase -o yaml | grep passphrase: | awk '{print $2}' | base64 --decode)
-    echo $PASSPHRASE | gpg --pinentry loopback --import --passphrase-fd 0 $(ls gpg/*.prv.key)
+    echo $PASSPHRASE | gpg --pinentry loopback --import --passphrase-fd 0 $(find ${OCD_CHECKOUT_PATH} -type f -name \*.prv.key)
     # check the key is there
     gpg --list-secret-keys | grep $FINGERPRINT
     if [ "$?" -ne "0" ]; then
-        >&2 echo "ERROR: Could not import key from $(ls gpg/*.prv.key)"
+        >&2 echo "ERROR: Could not import key from $(find ${OCD_CHECKOUT_PATH} -type f -name \*.prv.key)"
         exit 2
     fi
 fi
